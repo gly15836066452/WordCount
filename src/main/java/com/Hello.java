8ea192153c8921bdf6d819ebd9365f35a873a436
gly <1244376610@qq.com>
2021-02-23 17:12:44 +0800

    hi1

---
+++ src/main/java/com/Hello.java
@@ -0,0 +1,12 @@
+package com;
+
+/**
+ * @author ：
+ * @date ：2021/2/23
+ * @description：
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
